0d07897217ef1b5c90f21b973789684d6ee1455e
devKya <kihaok@gmail.com>
2022-11-17 17:10:20 +0900

    Finish App bar

--- frontend/src/components/common/MenuBar.vue
+++ frontend/src/components/common/MenuBar.vue
@@ -1,29 +1,20 @@
 <template>
 	<div>
-		<v-app-bar color="#494949" fixed app>
-			<v-icon class="px-3" dark large>mdi-basketball</v-icon>
-			<!-- <v-toolbar-title class="font-weight-bold text-h5" style="color: #fbbd28">
-				VUE - DJANGO DevKya WEB
-			</v-toolbar-title> -->
+		<v-app-bar color="#494949" clipped-right app>
+			<v-icon class="px-5" dark large>mdi-basketball</v-icon>
+			<v-toolbar-title class="font-weight-bold text-h5" style="color: #fbbd28">
+				VUE - DJANGO WEB
+			</v-toolbar-title>
 			<v-spacer></v-spacer>
 			<!-- display breakpoint -->
 			<!-- display lg down -->
-			<v-menu rounded offset-y>
-				<template v-slot:activator="{ attrs, on }">
-					<v-app-bar-nav-icon
-						class="hidden-lg-and-up"
-						v-bind="attrs"
-						v-on="on"
-						dark
-					></v-app-bar-nav-icon>
-				</template>
 
-				<v-list>
-					<v-list-item v-for="(item, index) in menu" :key="index" link>
-						<v-list-item-title v-text="item.title"></v-list-item-title>
-					</v-list-item>
-				</v-list>
-			</v-menu>
+			<v-app-bar-nav-icon
+				class="hidden-lg-and-up"
+				@click.stop="drawer = !drawer"
+				dark
+			>
+			</v-app-bar-nav-icon>
 
 			<!-- display lg up -->
 			<v-slide-group class="hidden-lg-and-down">
@@ -45,6 +36,31 @@
 				</v-slide-item>
 			</v-slide-group>
 		</v-app-bar>
+
+		<v-navigation-drawer
+			v-model="drawer"
+			clipped
+			right
+			:style="{ top: $vuetify.application.top + 'px', zIndex: 4 }"
+			app
+		>
+			<v-list rounded>
+				<v-list-item
+					class="pa-3"
+					v-for="(item, index) in menu"
+					:key="index"
+					link
+				>
+					<v-list-item-icon>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-item-icon>
+
+					<v-list-item-content>
+						<v-list-item-title>{{ item.title }}</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
 	</div>
 </template>
 <script>
@@ -53,11 +69,12 @@ export default {
 	components: {},
 	data() {
 		return {
+			drawer: false,
 			menu: [
-				{ icon: 'HOME', title: 'HOME', to: '' },
-				{ icon: 'BLOG', title: 'BLOG', to: '' },
-				{ icon: 'IAM', title: 'IAM', to: '' },
-				{ icon: 'ADMIN', title: 'ADMIN', to: '' },
+				{ icon: 'mdi-home', title: 'HOME', to: '' },
+				{ icon: 'mdi-web', title: 'BLOG', to: '' },
+				{ icon: 'mdi-account-circle', title: 'IAM', to: '' },
+				{ icon: 'mdi-check', title: 'ADMIN', to: '' },
 			],
 		};
 	},
---
+++ frontend/src/components/common/MenuBarTest.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+		<v-app-bar clipped-right flat dark app>
+			<v-spacer></v-spacer>
+		</v-app-bar>
+
+		<v-navigation-drawer clipped right flat dark app>
+			<v-list>
+				<v-list-item class="px-2">
+					<v-list-item-avatar>
+						<v-img src="https://randomuser.me/api/portraits/men/11.jpg"></v-img>
+					</v-list-item-avatar>
+
+					<v-list-item-title>John Doe</v-list-item-title>
+				</v-list-item>
+				<v-list-item
+					v-for="item in navbarlist"
+					:key="item.route"
+					:to="item.route"
+				>
+					<v-list-item-icon>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-item-icon>
+					<v-list-item-content>{{ item.text }}</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+	</div>
+</template>
+<script>
+export default {
+	name: 'MenuBar',
+	components: {},
+	data() {
+		return {
+			drawer: false,
+			menu: [
+				{ icon: 'HOME', title: 'HOME', to: '' },
+				{ icon: 'BLOG', title: 'BLOG', to: '' },
+				{ icon: 'IAM', title: 'IAM', to: '' },
+				{ icon: 'ADMIN', title: 'ADMIN', to: '' },
+			],
+		};
+	},
+	setup() {},
+	created() {},
+	mounted() {},
+	unmounted() {},
+	methods: {},
+};
+</script>
+<style scoped>
+#menu-btn::before {
+	background-color: transparent !important;
+}
+</style>
